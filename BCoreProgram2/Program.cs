37af26843503cb0344e1e40fe222c80de9092bd4
Vinaya2092000 <vinaya2srinivasan@gmail.com>
2022-08-28 15:37:37 +0530

    main

---
+++ BCoreProgram2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace BCoreProgram2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sh = Convert.ToInt32(Console.ReadLine());
+            NewMethod(sh);
+        }
+
+        private static void NewMethod(int sh)
+        {
+            switch (sh)
+            {
+                case 1:
+                    break;
+            }
+        }
+    }
+}
