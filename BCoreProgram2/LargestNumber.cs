4f1d4cf47371723c6089ad1c1962c9bbc57ab4fc
Vinaya2092000 <vinaya2srinivasan@gmail.com>
2022-08-28 17:47:04 +0530

    [Vinaya]Add: Largest of three

---
+++ BCoreProgram2/LargestNumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCoreProgram2
+{
+    public class LargestNumber
+    {
+        public void Three()
+        {
+            int a, b, c;
+
+            Console.Write("1st number :");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("2nd number :");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("3rd number :");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b)
+            {
+                if (a > c)
+                {
+                    Console.Write("The 1st Number is the greatest.\n");
+                }
+                else
+                {
+                    Console.Write("The 3rd Number is the greatest.\n");
+                }
+            }
+            else if (b > c)
+                Console.Write("The 2nd Number is the greatest.\n");
+            else
+                Console.Write("The 3rd Number is the greatest.\n");
+        }
+
+    }
+}
+
---
+++ BCoreProgram2/VowelsConsonents.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCoreProgram2
+{
+    public class VowelsConsonents
+    {
+        public void VowelCons()
+        {
+            char ch;
+
+            Console.WriteLine("Enter an Alphabet : ");
+            char v = Convert.ToChar(Console.ReadLine().ToLower());
+            ch = v;
+            int i = ch;
+            if (i >= 48 && i <= 57)
+            {
+                Console.Write("You entered a number, Please enter an alpahbet.");
+            }
+            else
+            {
+                switch (ch)
+                {
+                    case 'a':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'i':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'o':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'u':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'e':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    default:
+                        Console.WriteLine("The Alphabet is a Consonant");
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
