c40778bb14e5e4962a2d3abb7bead630c9d6779e
Vinaya2092000 <vinaya2srinivasan@gmail.com>
2022-08-28 16:03:29 +0530

    [Vinaya]Add: Welcome

--- BCoreProgram2/Program.cs
+++ BCoreProgram2/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter 1 for Quotient and Reminder");
+            Console.WriteLine("Enter 2 for Swap Numbers");
             int sh = Convert.ToInt32(Console.ReadLine());
             NewMethod(sh);
         }
@@ -13,6 +15,13 @@
             switch (sh)
             {
                 case 1:
+                    QuotientAndReminder qar = new QuotientAndReminder();
+                    qar.Divide();
+                    break;
+
+                case 2:
+                    SwapNumbers swap = new SwapNumbers();
+                    swap.SNumbers();
                     break;
             }
         }
---
+++ BCoreProgram2/SwapNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCoreProgram2
+{
+    public class SwapNumbers
+    {
+        public void SNumbers()
+        {
+            int n1, n2, temp;
+            Console.Write("First Number : ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Second Number : ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            temp = n1;
+            n1 = n2;
+            n2 = temp;
+            Console.Write("\nAfter Swapping : ");
+            Console.Write("\nFirst Number : " + n1);
+            Console.Write("\nSecond Number : " + n2);
+            Console.Read();
+        }
+    }
+}
